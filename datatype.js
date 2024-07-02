3bf8efc35bb5198dece94f28e420400f0ae27cfd
Deepan Kumar <145337793+dipankumar1098@users.noreply.github.com>
2024-07-02 18:26:59 +0530

    datatype

---
+++ datatype.js
@@ -0,0 +1,8 @@
+var brand= "peter england";
+var price=399;
+var avail=true;
+var rating=5;
+console.log(typeof price);
+console.log(typeof brand);
+console.log(typeof avail);
+console.log(typeof rating);
