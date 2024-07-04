1ba16a7134a3dc09b3a53f4d54e98904598682e6
Deepan Kumar <145337793+dipankumar1098@users.noreply.github.com>
2024-07-04 18:11:59 +0530

    upd

--- Operater.js
+++ Operater.js
@@ -3,6 +3,7 @@ let b=20.5;
 let c="deep";
 let d=true;
 let e=false;
+let f='10';
 
 let quantity='2';
 let price=399;
@@ -13,6 +14,8 @@ console.log(a+c);
 console.log(a+d);
 console.log(a+e);
 console.log(c+d);
+console.log(a==f);
+console.log(a===f);
 
-console.log(quantity+price);
-console.log(quantity*price);
+
+
