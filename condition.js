9dee0627e40648c3c950d4df9b32f957f7445c69
Deepan Kumar <145337793+dipankumar1098@users.noreply.github.com>
2024-07-04 18:29:07 +0530

    cond

---
+++ condition.js
@@ -0,0 +1,13 @@
+let sal = 45000;
+if(sal>30000)
+    {
+        console.log("eligible");
+    }
+    if(sal>30000)
+        {
+            console.log("eligible");
+        }
+        else
+        {
+            console.log("not eligible")
+        }
