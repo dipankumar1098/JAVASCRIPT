547f1035aee7bb293076f8dba7ef7882d2692513
Deepan Kumar <145337793+dipankumar1098@users.noreply.github.com>
2024-07-09 18:03:56 +0530

    add

---
+++ Array.js
@@ -0,0 +1,10 @@
+// using for loop
+let i=0;
+let eids=[101,102,103,104];
+
+for(i=0;i<=3;i++)
+{
+    console.log(eids[i])
+}
+
+//using while loop
--- Iterative.js
+++ Iterative.js
@@ -1,6 +1,20 @@
+//while loop
 let i=1;
 while(i<=5)
 {
     console.log(i);
     i=i+1
 }
+
+//for loop
+for(i=1;i<=5;i++)
+{
+    console.log(i)
+}
+
+//do while loop
+do{
+    console.log(i)
+    i=i+1
+}
+while(i<5)
