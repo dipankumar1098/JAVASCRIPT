aaf7e4872da961bbf4aa3957e79490c6667d1f36
Deepan Kumar <145337793+dipankumar1098@users.noreply.github.com>
2024-07-09 18:13:50 +0530

    Array

--- Array.js
+++ Array.js
@@ -2,9 +2,20 @@
 let i=0;
 let eids=[101,102,103,104];
 
-for(i=0;i<=3;i++)
+ for(i=0;i<=3;i++)
 {
     console.log(eids[i])
 }
 
 //using while loop
+while(i<=3)
+{
+    console.log(eids[i]);
+    i=i+1
+}
+//using do while loop 
+do{
+    console.log(eids[i])
+    i=i+1;
+}
+while(i<=3);
