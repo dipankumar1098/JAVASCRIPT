a4dd173bae3f6ea9595e4a9d4f9bc50452a0dddc
Deepan Kumar <145337793+dipankumar1098@users.noreply.github.com>
2024-07-11 16:17:36 +0530

    updated

---
+++ forinloop.js
@@ -0,0 +1,12 @@
+ 
+const person = { name: "Alice", age: 30 };
+
+for (const key in person) {
+  console.log(key + ": " + person[key]); 
+}
+  
+/* 
+  const person = [{ name: "Alice", age: 30 },{ name: "babul", age: 23 }];
+for (const temp          of person) {
+    console.log(temp.name+":"+temp.age);
+} */
