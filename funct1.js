447c1b1fbae1e1b5b6fd48825983b49a9fe798b0
Deepan Kumar <145337793+dipankumar1098@users.noreply.github.com>
2024-07-12 18:56:36 +0530

    add

---
+++ funct1.js
@@ -0,0 +1,10 @@
+function wish(){
+    console.log("Good Morning")
+    console.log("Good Afternoon")
+    console.log("Good Evening")
+}
+wish()
+wish()
+wish()
+wish()
+wish()
---
+++ two.js
@@ -0,0 +1,11 @@
+let ename=["deepan", "dipty" , "Ankita"];
+/* ename.reverse();
+console.log(ename) */
+
+//push
+/* ename.push("Sriya");
+console.log(ename); */
+
+//index of 
+
+console.log(ename.indexOf("dipty"));
