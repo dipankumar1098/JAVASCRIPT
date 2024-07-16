94a587aa912757d73b0383cdd4be08a8bf9363eb
Deepan Kumar <145337793+dipankumar1098@users.noreply.github.com>
2024-07-16 11:56:34 +0530

    add

--- DOM.js
+++ DOM.js
@@ -2,7 +2,7 @@ document.getElementById('abc').innerHTML ="Good Evening"
 
 
 function change_Color1(){
-    //alert('Test')
-    //document.getElementById('xyz').innerHTML="Test Case123"
-    document.getElementsByTagName('button')[0].style.backgroundColor='yellow'
+    alert('Test')
+    document.getElementById('xyz').innerHTML="Test Case123"
+/*     document.getElementsByTagName('button')[0].style.backgroundColor='yellow' */
 }
---
+++ table.js
@@ -0,0 +1,9 @@
+let employees=[{id:101,name:rahul,sal:45000},
+               {id:101,name:rahul,sal:45000},
+               {id:101,name:rahul,sal:45000}]
+
+               function display_EmployeeData(){
+                let rows=""
+                for
+
+               }
