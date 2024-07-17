1214008f56122cfd39747b35159dd00a238dd84f
Deepan Kumar <145337793+dipankumar1098@users.noreply.github.com>
2024-07-17 13:13:29 +0530

    add

--- fatarrow.js
+++ fatarrow.js
@@ -1,6 +1,15 @@
-
+/* 
 let add=(a,b)=>{
 
             console.log(a+b)
+            }
+            add(10,20) */
+
+
+
+
+            function add(a,b)
+            {
+                console.log(a+b);
             }
             add(10,20)
