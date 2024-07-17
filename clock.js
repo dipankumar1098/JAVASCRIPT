c7fad09b89f3aab77c49466afdb82d2bbbf752ce
Deepan Kumar <145337793+dipankumar1098@users.noreply.github.com>
2024-07-17 17:51:19 +0530

    add

---
+++ clock.js
@@ -0,0 +1,1 @@
+document.getElementById("p_Tag").innerHTML= newDate();
--- fatarrow.js
+++ fatarrow.js
@@ -1,15 +1,20 @@
-/* 
-let add=(a,b)=>{
+
+/* let add=(a,b)=>{
 
             console.log(a+b)
             }
             add(10,20) */
-
-
-
 
             function add(a,b)
             {
                 console.log(a+b);
             }
             add(10,20)
+
+    /* 
+            function add(a,b)
+            {
+                return a+b;
+            }
+            let r=add(20,30);
+            console.log(r); */
