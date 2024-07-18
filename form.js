d2fb8fe04c212fc87655499288c37f78f4dc170a
Deepan Kumar <145337793+dipankumar1098@users.noreply.github.com>
2024-07-18 18:21:49 +0530

    add

--- form.js
+++ form.js
@@ -1,5 +1,14 @@
-function validate()
-{
-    let email= document.getElementById('email').value
-    let password= document.getElementById('pass').value
+function validateData(){
+    let email=document.getElementById('email').value;
+    let password=document.getElementById('pass').value;
+    //alert(email,password)
+    if(email =="" || email == null){
+        //update span tab
+        document.getElementById('emailmsg').innerHTML="Please Enter Email Id"
+    }
+    
+    if(password=="" || password==null){
+        document.getElementById('pwdmsg').innerHTML ="Pls enter Password"
+    }
+    return false
 }
