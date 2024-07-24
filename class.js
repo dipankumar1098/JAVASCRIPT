a2fe8e04d03e79ff8b4f365237ef9c9b0fcde3e0
Deepan Kumar <145337793+dipankumar1098@users.noreply.github.com>
2024-07-24 17:40:24 +0530

    add

--- class.js
+++ class.js
@@ -1,8 +1,8 @@
 class acc{
-    acc.bal=0;
-    min.bal=500;
+    min_bal=0;
+    acc_bal=500;
 }
-let a1=new Account();
+let a1=new_Account();
 console.log(a1);
-let a2=new Account();
+let a2 =deposit_Account();
 console.log(a2)
