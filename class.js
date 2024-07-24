0636658459a771ad3cb1483f704b2aee2ef1814e
Deepan Kumar <145337793+dipankumar1098@users.noreply.github.com>
2024-07-24 17:41:06 +0530

    add

--- class.js
+++ class.js
@@ -1,8 +1,14 @@
-class acc{
-    min_bal=0;
-    acc_bal=500;
+class Account{
+    min_Bal=500;
+    acc_Bal=0;
+    deposit_Amount(amount){
+        this.acc_Bal = this.acc_Bal+amount
+    }
 }
-let a1=new_Account();
-console.log(a1);
-let a2 =deposit_Account();
+let a1=new Account();
+console.log(a1)
+a1.deposit_Amount(15000)
+a1.deposit_Amount(15)
+console.log(a1)
+let a2=new Account();
 console.log(a2)
