6b784a60842e960fd5001daf143af9d065844f45
Deepan Kumar <145337793+dipankumar1098@users.noreply.github.com>
2024-07-25 16:29:39 +0530

    add

--- object.js
+++ object.js
@@ -1,5 +1,23 @@
 class employee
 {
+    emp_id=101
+    emp_sal=20000
 
+    emp_id=102
+    emp_sal=30000
+
+    emp_id=103
+    emp_sal=40000
+
+    emp_id=104
+    emp_sal=50000
+    
 }
-console . log(new employee());
+var a=new employee()
+var b=new employee()
+var c=new employee()
+var d=new employee()
+console . log(a);
+console . log(b);
+console . log(c);
+console . log(d);
