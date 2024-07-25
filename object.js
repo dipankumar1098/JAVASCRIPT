189067dd325d0f88b6b9399d0e02233642b154d9
Deepan Kumar <145337793+dipankumar1098@users.noreply.github.com>
2024-07-25 17:01:48 +0530

    add

--- object.js
+++ object.js
@@ -2,16 +2,6 @@ class employee
 {
     emp_id=101
     emp_sal=20000
-
-    emp_id=102
-    emp_sal=30000
-
-    emp_id=103
-    emp_sal=40000
-
-    emp_id=104
-    emp_sal=50000
-    
 }
 var a=new employee()
 var b=new employee()
