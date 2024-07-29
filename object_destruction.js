f064c568e2f8e0312fe91a174898fae656de79ae
Deepan Kumar <145337793+dipankumar1098@users.noreply.github.com>
2024-07-29 18:22:11 +0530

    add

---
+++ object_destruction.js
@@ -0,0 +1,11 @@
+let emp={
+            id:'101',
+            Name:"Deepan_kumar",
+            email:'deepan@gmail.com'
+        }
+let details={
+            salary:'25000',
+            email :'deepan@gmail.com'
+        }
+let emp_details={...emp , ...details}
+console.log(emp_details);
