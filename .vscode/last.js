f598e8dad3e9fa8c10f73d54090e544e90747d86
Deepan Kumar <145337793+dipankumar1098@users.noreply.github.com>
2024-08-03 11:43:12 +0530

    add

---
+++ .vscode/last.js
@@ -0,0 +1,8 @@
+let createEmployee=()=>{
+    setTimeout(()=>{console.log("gm")},4000)
+                    }
+let getEmployee=()=>{
+    setTimeout(()=>{console.log("gA")},2000)
+                    }
+                    createEmployee()
+                    getEmployee()
